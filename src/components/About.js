068e695562c9e8ea772db52219a4bae03672187d
jashonmichael <jashon.michael@student.moringaschool.com>
2024-04-24 11:57:29 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function About() {
+function About({bio}) {
   return (
     <div id="about">
       <h2>About Me</h2>
+      {bio && <p>{bio}</p>}
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
@@ -11,4 +12,14 @@ function About() {
   );
 }
 
-export default About;
+function Links({ githubUrl, linkedinUrl }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <ul>
+        <li><a href={githubUrl}>{githubUrl}</a></li>
+        <li><a href={linkedinUrl}>{linkedinUrl}</a></li>
+      </ul>
+    </div>
+  );
+}
